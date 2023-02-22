45d41d0c59fa23d55ebf6adfad1074360c5cfdac
Olivia Busk <123026517+obusk01@users.noreply.github.com>
2023-02-22 15:56:21 -0500

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,27 @@
+# Test 1: Olivia Busk
+# Part II
+
+# Mid 15
+for rps in ["paper", "rock", "paper","scissor", "rock","rock","scissor","paper"]:
+    if rps=="paper":
+        print("paper eats rock")
+    if rps=="scissor":
+        print("scissor beats paper")
+    else:
+        print("rock hits scissor")
+
+# Mid 14
+def next_stop(location):
+    print("The next stop is", location)
+    print("The next stop is", location)
+
+next_stop("South Bend")
+
+# Mid 12
+def if_divisible_five(x):
+    if x % 5 ==0:
+        print("Yes, it is!")
+    else:
+        print("No, it is not!")
+if_divisible_five(45)
+if_divisible_five(782)
